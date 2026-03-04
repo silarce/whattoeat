f33b2b44c87cbb52e99673b62f80dc85457c2da6
Silarce <silarceanoux@gmail.com>
2026-03-04 23:48:34 +0800

    新增 MapPinIcon 和 InfoIcon 元件，並在餐廳列表中替換相應的圖示

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -3,3 +3,5 @@ export { XCircleIcon } from "./XCircleIcon";
 export { MenuIcon } from "./MenuIcon";
 export { SunIcon } from "./SunIcon";
 export { MoonIcon } from "./MoonIcon";
+export { MapPinIcon } from "./MapPinIcon";
+export { InfoIcon } from "./InfoIcon";
