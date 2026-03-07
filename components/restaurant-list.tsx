f29d5f46ba923c1b67619fce2bda029af23b81fa
Silarce <silarceanoux@gmail.com>
2026-03-07 15:17:52 +0800

    更新餐廳列表提示文字，當無餐廳時顯示「附近沒有餐廳」

--- components/restaurant-list.tsx
+++ components/restaurant-list.tsx
@@ -69,7 +69,7 @@ export function RestaurantList({
       <CardBody className="flex-1 min-h-0 pb-0 flex flex-col">
         {totalCount === 0 ? (
           <div className="flex h-32 items-center justify-center rounded-xl border-2 border-dashed border-gray-200 dark:border-gray-700">
-            <p className="text-sm text-gray-500 dark:text-gray-400">點擊「搜尋附近餐廳」開始探索</p>
+            <p className="text-sm text-gray-500 dark:text-gray-400">附近沒有餐廳</p>
           </div>
         ) : (
           <>
