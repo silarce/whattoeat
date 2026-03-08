d21fad923a141f175690f97d9f547bdd7682ddb8
Silarce <silarceanoux@gmail.com>
2026-03-08 19:23:14 +0800

    補上cursor-pointer

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -18,11 +18,11 @@ function Card({ name, isSelected, disabled, onClick, onRemove }: WheelCardProps)
       onClick={onClick}
       disabled={disabled}
       className={cn(
-        "relative w-full overflow-hidden rounded-xl border-2 px-3 py-3 text-center transition-all duration-150",
+        "relative w-full overflow-hidden rounded-xl border-2 px-3 py-3 text-center transition-all duration-150 cursor-pointer",
         disabled && "pointer-events-none select-none opacity-80",
         isSelected
-          ? "border-orange-500 bg-orange-50 shadow-lg shadow-orange-100 dark:bg-orange-950 dark:shadow-orange-900/20"
-          : "border-gray-100 bg-gray-50 hover:border-orange-300 hover:bg-orange-50/50 cursor-pointer dark:border-gray-700 dark:bg-gray-800 dark:hover:border-orange-600 dark:hover:bg-orange-950/50",
+          ? "border-orange-500 bg-orange-50 shadow-lg shadow-orange-100 dark:bg-orange-950 dark:shadow-orange-900/20 "
+          : "border-gray-100 bg-gray-50 hover:border-orange-300 hover:bg-orange-50/50 dark:border-gray-700 dark:bg-gray-800 dark:hover:border-orange-600 dark:hover:bg-orange-950/50",
       )}
     >
       {onRemove && (
